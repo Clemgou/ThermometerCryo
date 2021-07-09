247957c7a7c34bd9a40fdeea01cbc9a9bcf5ffd5
Clemgou <clementgouriou@orange.fr>
2021-07-09 16:44:05 +0200

    Some correction.

--- lib/Conversion_functions.py
+++ lib/Conversion_functions.py
@@ -259,6 +259,65 @@ def ThermoBT_TvsR_spln(R, above70K=False, **kwargs):
     # ---  --- #
     return T[0] if T.size == 1 else T
 
+def ThermoHT_TvsR_spln(R, above70K=False, **kwargs):
+    '''
+    * Inverse of ThermoHT_RvsT, in order to get the temperature from a resistance measurement.
+      However, one must take the distinction between the part above 70 K, and the one bellow 70K.
+    * Without any analytical inverse function, the evaluation of T=func(R) is made using approximation
+      cubic spline functions, defines in specific range of R. It commes from a manual optimisation, and
+      the good enough spline coefficient are stored in the function.
+    * The spline optimisation has been made using cubicSpline_coeff_computing.
+    '''
+    verbose  = kwargs.pop('verbose', False)
+    dR_bnd   = kwargs.pop('dR_bnd' , 1e-5) # [Ohm]
+    T_thrs   = kwargs.pop('T_thrs' ,78.88)
+    # --- Spline coefficient and domain definition --- #
+    spln_dic = {
+        0 : {'above70K':True , 'boundary': [ThermoHT_RvsT(78.88),  150.61524966725247], 'coeff': (78.87, 56.1844528190399, -115.21650595296315, 92.91011161676585)},
+        1 : {'above70K':True , 'boundary': [ 150.61524966725247 ,  151.78900178948638], 'coeff': (90.0, 9.803150644715748, -0.9541498222969034, -0.11869751514322234)},
+        2 : {'above70K':True , 'boundary': [ 151.78900178948638 ,  178.07012929774106], 'coeff': (100.0, 6.44617832053299, -0.17477612688316743, 0.002826341030047476)},
+        3 : {'above70K':True , 'boundary': [ 178.07012929774106 ,  349.907262631601  ], 'coeff': (200.0, 3.0852288333006377, -0.0026543562764718516, 9.503525585446059e-06)},
+        4 : {'above70K':True , 'boundary': [ 349.907262631601   ,  445.274437748751  ], 'coeff': (700.0, 3.0317556045995855, 0.000969502342940667, 2.366398942705002e-06)},
+        5 : {'above70K':True , 'boundary': [ 445.274437748751   , 1442.1881228478064 ], 'coeff': (1000.0, 3.3104272074809544, 0.003208419044933503, -2.0073843666710245e-06)},
+        6 : {'above70K':True , 'boundary': [1442.1881228478064  , ThermoHT_RvsT(1e4) ], 'coeff': (5500.0, 3.538239255543073, -0.0011155413449810807, 1.5858861298707488e-07)},
+        7 : {'above70K':False, 'boundary': [ThermoHT_RvsT(78.88),    150.63460484496625], 'coeff': (78.89, -49.68664219148569, 98.12112288640778, -74.13358972012989)},
+        8 : {'above70K':False, 'boundary': [  150.63460484496625,    152.08990434374232], 'coeff': (69.0, -6.996422594399698, 0.9709034007228218, 0.04075605632432042)},
+        9 : {'above70K':False, 'boundary': [  152.08990434374232,    166.90080016951902], 'coeff': (61.0, -3.459361554206645, 0.20552366262672184, -0.005185654321098554)},
+        10: {'above70K':False, 'boundary': [  166.90080016951902,    195.3079427383702 ], 'coeff': (38.0, -0.7325487027356796, 0.01299398006621569, -0.00011673918843419716)},
+        11: {'above70K':False, 'boundary': [  195.3079427383702 ,    275.4114419501825 ], 'coeff': (25.0, -0.2605281681704082, 0.0022282978159015687, -8.616538071755353e-06)},
+        12: {'above70K':False, 'boundary': [  275.4114419501825 ,    453.72525498106404], 'coeff': (14.0, -0.06518297122915089, 0.00025748929720128286, -4.5224409957563403e-07)},
+        13: {'above70K':False, 'boundary': [  453.72525498106404,    817.5436713623418 ], 'coeff': (8.0, -0.015373704581517638, 2.8043857128286924e-05, -2.3231713819859912e-08)},
+        14: {'above70K':False, 'boundary': [  817.5436713623418 ,   1998.6568610417357 ], 'coeff': (5.0, -0.0038667687689848936, 2.8542271727440443e-06, -8.585591732787398e-10)},
+        15: {'above70K':False, 'boundary': [ 1998.6568610417357 ,   5433.732919117695  ], 'coeff': (3.0, -0.0006492945639255994, 1.6059978003971705e-07, -1.6398000404073262e-11)},
+        16: {'above70K':False, 'boundary': [ 5433.732919117695  ,  18759.700620748117  ], 'coeff': (2.0, -0.00011146840690453101, 7.938633457490856e-09, -2.2156844989306396e-13)},
+        17: {'above70K':False, 'boundary': [18759.700620748117  ,  60276.239081227555  ], 'coeff': (1.4, -1.5172329659141793e-05, 2.86508463044623e-10, -2.290840867286958e-15)},
+        18: {'above70K':False, 'boundary': [60276.239081227555  ,ThermoHT_RvsT(1e0+dR_bnd)], 'coeff': (1.1, -2.598913621689561e-06, 5.380814434880268e-12, 1.0165656652245034e-16)},
+    }
+    # ---  --- #
+    R_ = np.array(R, ndmin=1)
+    T  = np.zeros(R_.shape)
+    # --- Set out of range default values
+    T[R_>ThermoHT_RvsT(1e+4)  ] += 1e4*above70K
+    T[R_>ThermoHT_RvsT(1e0 )  ] += 1e-3*(not above70K)
+    T[R_<ThermoHT_RvsT(T_thrs)]  = np.nan
+    # --- Computing T from splines
+    for spl_key in spln_dic:
+        R_b     = spln_dic[spl_key]['boundary']
+        a,b,c,d = spln_dic[spl_key]['coeff']
+        # ---
+        idx_g   = np.where(R_b[0]-dR_bnd<=R_)[0]
+        idx_l   = np.where(R_<=R_b[1]+dR_bnd)[0]
+        idx     = list(set(idx_g).intersection(set(idx_l)))
+        # ---
+        T[idx] += (a + b*(R_[idx]-R_b[0])**1 + c*(R_[idx]-R_b[0])**2 + d*(R_[idx]-R_b[0])**3) * int(spln_dic[spl_key]['above70K']==above70K)
+    # ---  --- #
+    return T[0] if T.size == 1 else T
+
+
+
+
+
+
 
 
 
